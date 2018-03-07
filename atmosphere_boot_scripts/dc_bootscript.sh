879380801c4754e8c297d1cfba4c51babbcc7400
Jason Williams <JasonJWilliamsNY@gmail.com>
2018-03-06 21:22:38 -0500

    update bootscript

--- atmosphere_boot_scripts/dc_bootscript.sh
+++ atmosphere_boot_scripts/dc_bootscript.sh
@@ -36,6 +36,8 @@ copy_dc_home_datasets ()
 {
   # Performs an irsync command to copy a test dataset folder to $ATMO_USER
   # desktop
+  mkdir /home/dcuser
+  chown -R dcuser /home/dcuser
   irsync -rs i:/iplant/home/shared/cyverse_training/workshop_materials/genomics_data_carpentry_1_0_release/ /scratch >/var/log/williams_bootscript.log 2>&1
   chown -R dcuser /scratch
   cp -rs /scratch/* /home/dcuser
