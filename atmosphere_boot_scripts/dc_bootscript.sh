8c06f0dd04c21bd4a7fb0dea995b75e4d608573c
Jason Williams <JasonJWilliamsNY@gmail.com>
2018-03-06 21:55:14 -0500

    update sym link to hidden directory

--- atmosphere_boot_scripts/dc_bootscript.sh
+++ atmosphere_boot_scripts/dc_bootscript.sh
@@ -36,9 +36,11 @@ copy_dc_home_datasets ()
 {
   # Performs an irsync command to copy a test dataset folder to $ATMO_USER
   # desktop
+  rm DATA-LOSS-WARNING.txt
   chown -R dcuser /home/dcuser
   irsync -rs i:/iplant/home/shared/cyverse_training/workshop_materials/genomics_data_carpentry_1_0_release/ /scratch >/var/log/williams_bootscript.log 2>&1
   chown -R dcuser /scratch
+  cp -rs /scratch/.dc_sampledata_lite /home/dcuser
   cp -rs /scratch/* /home/dcuser
   chown -R dcuser /home/dcuser
 
